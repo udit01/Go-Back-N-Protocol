2d87693112b7a4dc30d2997284593dd29cf308f6
udit01 <uditjainiitd@gmail.com>
2018-09-27 01:19:54 +0530

    Added packet constraints

--- Packet.py
+++ Packet.py
@@ -4,10 +4,19 @@ class DataFrame():
     """
     Packet class for go back n protocol 
     """
+    # everything is in bits
+    HEADER_SIZE = 256
+    MIN_PAYLOAD_SIZE = 512 - HEADER_SIZE 
+    MAX_PAYLOAD_SIZE = 2024 - HEADER_SIZE
+
+
+
     pass
 
 class AckFrame():
     """
     Ack class 
     """
+    HEADER_SIZE = 256
+    PAYLOAD_SIZE = 0
     pass
